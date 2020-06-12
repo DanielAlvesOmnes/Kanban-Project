95e4ca2b2b487686e003866ba67f3ad9d1d30fc0
Daniel Alves <daniel_alves_santos@hotmail.com>
2020-06-12 14:36:37 -0300

    Kanban Project

---
+++ src/Components/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import TrelloList from './TrelloList';
+import { connect } from 'react-redux';
+import TrelloActionButton from './TrelloActionButton';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { sort } from '../Actions';
+import styled from 'styled-components';
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row
+`;
+
+class App extends Component {
+
+  onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result;
+
+    if(!destination) {
+      return;
+    }
+
+    this.props.dispatch (
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId,
+        type,
+      )
+    );
+
+  };
+   
+  render () {
+const { lists } = this.props;
+  
+  return (
+    <DragDropContext onDragEnd={this.onDragEnd}>
+    <div>
+     <h2>Hello World</h2>
+     <Droppable droppableId='all-lists' direction='horizontal' type='list'>
+       {provided => (
+        <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
+     { (lists.map ((list, index) => 
+        <TrelloList 
+        listID={list.id} 
+        key={list.id} 
+        title={list.title} 
+        cards={list.cards}
+        index={index} />
+     ))}
+     <TrelloActionButton list />
+     </ListContainer>
+       )}
+     </Droppable>
+     
+    </div>
+    </DragDropContext>
+  );
+};
+};
+
+
+const MapStateToProps = state => ({
+  lists: state.lists
+})
+
+export default connect (MapStateToProps) (App);
